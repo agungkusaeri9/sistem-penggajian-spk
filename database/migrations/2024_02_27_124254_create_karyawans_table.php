7723e9165ea8010a691697a887760f4fb23277a9
Agung Kusaeri <agung.kusaeri9@gmail.com>
2024-02-28 15:50:23 +0700

    feat(karyawan) -Membuat mvc karyawan

---
+++ database/migrations/2024_02_27_124254_create_karyawans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nik');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->string('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('nomor_telepon');
+            $table->foreignId('jabatan_id')->constrained('jabatan');
+            $table->foreignId('divisi_id')->constrained('divisi');
+            $table->foreignId('golongan_gaji_id')->constrained('golongan_gaji');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
